2758af9a8d0477cf8c0029f6cb55719f44222a82
ellislukas <ellis.lukas@valtech.com>
2020-01-15 15:06:47 +0000

    init

---
+++ mvcSite/ViewModels/Home/MovieOrderRow.cs
@@ -0,0 +1,23 @@
+﻿using mvcSite.Models;
+using mvcSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using mvcSite.CustomisedValidators;
+
+namespace mvcSite.ViewModels.Home
+{
+    public class MovieOrderRow
+    {
+        public int MovieID { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ mvcSite/ViewModels/Summary/SummaryRow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcSite.ViewModels.Summary
+{
+    public class SummaryRow
+    {
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
